7a0e1c9e4e61441a6f457414507543577377997d
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-21 12:52:59 -0400

    feat: Add currency symbols
    Adds new SVG icons for various currency symbols to the `assets/icons` directory.
    
    Specific changes:
    - Added `symbol_turkey_currency.svg` for the Turkish Lira.
    - Added `symbol_russian_currency.svg` for the Russian Ruble.
    - Added `symbol_china_currency.svg` for the Chinese Yuan.
    - Added `symbol_eeuu_currency.svg` for the US Dollar.
    - Added `symbol_european_currency.svg` for the Euro.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:bcv_tracker_app/features/splash/presentation/page/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'config/bindings/initial_bindings.dart';
 import 'config/language/translation.dart';
-import 'config/routes/pages.dart';
+import 'core/constants/constants.dart';
 
 void main() async {
   await dotenv.load(
@@ -17,36 +18,17 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tu Aplicación',
-
-      // --- Inyección de Dependencias Global ---
-      initialBinding: InitialBinding(), // Aquí se registra tu binding global
-
-      // --- Rutas ---
-      initialRoute: AppPages.initPage, // La ruta inicial de tu app
-      getPages: AppPages.routes,      // Tu lista de GetPage
-
-      // --- Tema ---
-      //theme: AppTheme.lightTheme,
-      //darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system, // O el que prefieras
-
-      // Si no quieres que GetX maneje el locale, puedes quitar esto.
+      title: Constants.appTitle,
+      initialBinding: InitialBinding(),
+      home: SplashPage(),
+      themeMode: ThemeMode.system,
       locale: Get.deviceLocale,
-      fallbackLocale: Locale('en', 'US'),
-      translations: AppTranslations(), // Si usas GetX para traducciones
-
-      // builder: (context, child) { // Puedes tener un builder global si es necesario
-      //   return MediaQuery(
-      //     data: MediaQuery.of(context).copyWith(textScaler: TextScaler.linear(1.0)), // Ejemplo
-      //     child: child!,
-      //   );
-      // },
+      fallbackLocale: const Locale('en', 'US'),
+      translations: AppTranslations(),
     );
   }
 }
