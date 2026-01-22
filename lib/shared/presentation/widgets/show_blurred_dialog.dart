a3f9feeffb29620790aa8fe210bf67d7b0fc4a2a
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-22 18:01:18 -0400

    feat: Add reusable blurred dialog function
    Introduces a new reusable function, `showBlurredDialog`, to display any widget as a dialog with a blurred background effect.
    
    This function uses `showGeneralDialog`, `BackdropFilter`, and `FadeTransition` to create a visually appealing and modern dialog presentation with a smooth fade-in animation. The blur intensity and background color are configurable.

---
+++ lib/shared/presentation/widgets/show_blurred_dialog.dart
@@ -0,0 +1,34 @@
+import 'dart:ui'; // Necesario para ImageFilter
+import 'package:bcv_tracker_app/config/theme/colors/colors_values.dart';
+import 'package:flutter/material.dart';
+
+// Función reutilizable para mostrar cualquier widget como un diálogo con fondo difuminado
+Future<T?> showBlurredDialog<T>({
+  required BuildContext context,
+  required Widget Function(BuildContext) builder,
+}) {
+  return showGeneralDialog<T>(
+    context: context,
+    // Duración de la animación de entrada y salida
+    transitionDuration: const Duration(milliseconds: 200),
+    // Define cómo se construye la animación de transición
+    transitionBuilder: (context, animation, secondaryAnimation, child) {
+      // Usa un FadeTransition para que el diálogo aparezca y desaparezca suavemente
+      return FadeTransition(
+        opacity: animation,
+        child: child,
+      );
+    },
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return BackdropFilter(
+        // El sigmaX y sigmaY controlan la intensidad del difuminado
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child: builder(context), // Aquí se construye tu AlertDialog
+      );
+    },
+    // Es importante hacer el fondo transparente para que se vea el contenido detrás del BackdropFilter
+    barrierColor: ColorValues.textBlack(context),
+    barrierDismissible: true,
+    barrierLabel: '', // Requerido por la API
+  );
+}
