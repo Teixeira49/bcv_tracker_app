6b4a0f8c505a822031dd17bc25a92c1af820e170
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-22 01:40:14 -0400

    feat(i18n): Add new translations and refactor message class
    This commit introduces new translation keys and their corresponding values across all supported languages. It also refactors the `AppMessages` class to use getters for improved code structure.
    
    Specific changes:
    - Added new translations for keys: `officialDollar`, `parallelDollar`, `moneyValue`, `currencyDate`, `eeuuDollar`, `europeanEuro`, `turkishLira`, `chineseYuan`, and `russianRuble`.
    - Updated all language files (`ru_ru`, `ko_kr`, `it_it`, `de_de`, `en_us`, `es_es`, `fr_fr`, `zh_cn`, `ja_jp`) with the new translations.
    - Refactored `AppMessages` to use getters for all message strings instead of static variables.
    - Introduced a new parameterized method `currencyDate({required String date})` for dynamic date formatting.

--- lib/core/i18n/app_messages.dart
+++ lib/core/i18n/app_messages.dart
@@ -1,55 +1,74 @@
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
 
 class AppMessages {
-  static String homeView = 'homeView'.tr;
+  static String get homeView => 'homeView'.tr;
 
-  static String converterView = 'converterView'.tr;
+  static String get converterView => 'converterView'.tr;
 
-  static String settingsView = 'settingsView'.tr;
+  static String get settingsView => 'settingsView'.tr;
 
-  static String averageSection = 'averageSection'.tr;
+  static String get averageSection => 'averageSection'.tr;
 
-  static String officialSection = 'officialSection'.tr;
+  static String get officialSection => 'officialSection'.tr;
 
-  static String averageValue = 'averageValue'.tr;
+  static String get averageValue => 'averageValue'.tr;
 
-  static String currencyValue = 'currencyValue'.tr;
+  static String get currencyValue => 'currencyValue'.tr;
 
-  static String lastUpdate = 'lastUpdate'.tr;
+  static String get lastUpdate => 'lastUpdate'.tr;
 
-  static String selectCurrency = 'selectCurrency'.tr;
+  static String get moneyValue => 'moneyValue'.tr;
 
-  static String defaultMarket = 'defaultMarket'.tr;
+  static String currencyDate({required String date}) =>
+      'currencyDate'.trParams({'date': date});
 
-  static String theme = 'theme'.tr;
+  static String get selectCurrency => 'selectCurrency'.tr;
 
-  static String language = 'language'.tr;
+  static String get defaultMarket => 'defaultMarket'.tr;
 
-  static String lightTheme = 'lightTheme'.tr;
+  static String get officialDollar => 'officialDollar'.tr;
 
-  static String darkTheme = 'darkTheme'.tr;
+  static String get parallelDollar => 'parallelDollar'.tr;
 
-  static String systemTheme = 'systemTheme'.tr;
+  static String get eeuuDollar => 'eeuuDollar'.tr;
 
-  static String defaultLanguage = 'defaultLanguage'.tr;
+  static String get europeanEuro => 'europeanEuro'.tr;
 
-  static String spanishLanguage = 'spanishLanguage'.tr;
+  static String get turkishLira => 'turkishLira'.tr;
 
-  static String englishLanguage = 'englishLanguage'.tr;
+  static String get chineseYuan => 'chineseYuan'.tr;
 
-  static String portugueseLanguage = 'portugueseLanguage'.tr;
+  static String get russianRuble => 'russianRuble'.tr;
 
-  static String germanLanguage = 'germanLanguage'.tr;
+  static String get theme => 'theme'.tr;
 
-  static String frenchLanguage = 'frenchLanguage'.tr;
+  static String get language => 'language'.tr;
 
-  static String italianLanguage = 'italianLanguage'.tr;
+  static String get lightTheme => 'lightTheme'.tr;
 
-  static String japaneseLanguage = 'japaneseLanguage'.tr;
+  static String get darkTheme => 'darkTheme'.tr;
 
-  static String koreanLanguage = 'koreanLanguage'.tr;
+  static String get systemTheme => 'systemTheme'.tr;
 
-  static String chineseLanguage = 'chineseLanguage'.tr;
+  static String get defaultLanguage => 'defaultLanguage'.tr;
 
-  static String russianLanguage = 'russianLanguage'.tr;
+  static String get spanishLanguage => 'spanishLanguage'.tr;
+
+  static String get englishLanguage => 'englishLanguage'.tr;
+
+  static String get portugueseLanguage => 'portugueseLanguage'.tr;
+
+  static String get germanLanguage => 'germanLanguage'.tr;
+
+  static String get frenchLanguage => 'frenchLanguage'.tr;
+
+  static String get italianLanguage => 'italianLanguage'.tr;
+
+  static String get japaneseLanguage => 'japaneseLanguage'.tr;
+
+  static String get koreanLanguage => 'koreanLanguage'.tr;
+
+  static String get chineseLanguage => 'chineseLanguage'.tr;
+
+  static String get russianLanguage => 'russianLanguage'.tr;
 }
