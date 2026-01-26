fd94bbb98608007ecbf6811deb9f8b6bd2786b01
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-26 19:04:48 -0400

    feat: Add theme support and internationalization
    Integrates theme management and internationalization into the application's entry point.
    
    Specific changes:
    - Adds `AppTheme.lightTheme` and `AppTheme.darkTheme` for theme support.
    - Initializes `SettingsController` using `Get.put()` to manage theme state.
    - Replaces the old `Translation` with `AppTranslations` for localization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
+import 'package:bcv_tracker_app/config/theme/theme.dart';
 import 'package:bcv_tracker_app/features/splash/presentation/page/splash_page.dart';
+import 'package:bcv_tracker_app/shared/presentation/controller/settings_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'config/bindings/initial_bindings.dart';
-import 'config/language/translation.dart';
 import 'core/constants/constants.dart';
+import 'core/i18n/app_translations.dart';
 
 void main() async {
   await dotenv.load(
     fileName: ".env",
   );
   WidgetsFlutterBinding.ensureInitialized();
+  Get.put(SettingsController());
   runApp(const MyApp());
 }
 
@@ -25,6 +28,8 @@ class MyApp extends StatelessWidget {
       title: Constants.appTitle,
       initialBinding: InitialBinding(),
       home: SplashPage(),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
       locale: Get.deviceLocale,
       fallbackLocale: const Locale('en', 'US'),
