f548fc5d88cfe1ddfc5e70619826c8f46d275b69
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-31 01:14:25 -0400

    refactor: Update Currency entity and add CurrencyCountry
    Refactors the `Currency` entity and introduces a new `CurrencyCountry` entity.
    
    Specific changes:
    - **`Currency` entity:**
        - Added new fields: `id`, `platform`, `createDate`, and `updateDate`.
        - Changed the type of `value` from `String` to `double`.
        - Renamed `date` to `updateDate` for clarity.
        - Updated the `copyWith` method and static instances (`empty`, `emptySkeletonizer`) to reflect the new structure.
        - Added a new static instance `pivotCurrency`.
    - **`CurrencyCountry` entity:**
        - Created a new entity to represent currency information related to a country, including `countryName`, `currencyCode`, `currencyCountryName`, `currencySymbol`, and `countryFlag`.

---
+++ lib/features/home/domain/entities/currency_country.dart
@@ -0,0 +1,15 @@
+class CurrencyCountry {
+  final String countryName;
+  final String currencyCode;
+  final String currencyCountryName;
+  final String currencySymbol;
+  final String countryFlag;
+
+  CurrencyCountry({
+    required this.countryName,
+    required this.currencyCode,
+    required this.currencyCountryName,
+    required this.currencySymbol,
+    required this.countryFlag,
+  });
+}
--- lib/shared/domain/entities/currency.dart
+++ lib/shared/domain/entities/currency.dart
@@ -1,43 +1,74 @@
 class Currency {
+  final int id;
   final String name;
   final String keyName;
-  final String value;
+  final String platform;
+  final double value;
   final String? imgUrl;
-  final DateTime? date;
+  final DateTime? createDate;
+  final DateTime? updateDate;
   final double? tendency;
 
   const Currency({
+    required this.id,
     required this.name,
     required this.keyName,
+    required this.platform,
     required this.value,
     this.imgUrl,
-    this.date,
+    this.createDate,
+    this.updateDate,
     this.tendency,
   });
 
   Currency copyWith({
     String? name,
     String? keyName,
-    String? value,
+    String? platform,
+    double? value,
     String? imgUrl,
-    DateTime? date,
+    DateTime? updateDate,
     double? tendency,
   }) {
     return Currency(
+      id: id,
       name: name ?? this.name,
       keyName: keyName ?? this.keyName,
+      platform: platform ?? this.platform,
       value: value ?? this.value,
       imgUrl: imgUrl ?? this.imgUrl,
-      date: date ?? this.date,
+      updateDate: updateDate ?? this.updateDate,
       tendency: tendency ?? this.tendency,
     );
   }
 
-  static const empty = Currency(name: '', keyName: '', value: '');
+  static const empty = Currency(
+    id: 0,
+    name: '',
+    platform: '',
+    keyName: '',
+    value: 0.00,
+    imgUrl: '',
+    tendency: 0.00,
+  );
 
-  static const emptySkeletonizer = Currency(
+  static final emptySkeletonizer = Currency(
     name: 'name',
     keyName: 'keyName',
-    value: '1.000',
+    platform: 'platform',
+    value: 1.0,
+    imgUrl: 'imgUrl',
+    createDate: DateTime.now(),
+    updateDate: DateTime.now(),
+    tendency: 1.0,
+    id: 0,
+  );
+
+  static final pivotCurrency = Currency(
+    keyName: 'VES',
+    name: 'Banco Central de Venezuela',
+    value: 1.0,
+    platform: 'Banco Central de Venezuela',
+    id: -1,
   );
 }
