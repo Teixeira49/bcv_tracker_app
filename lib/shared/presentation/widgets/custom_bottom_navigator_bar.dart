304a89fcab8a4e0374f8bc97f89505f2094a9a50
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-02-09 10:42:59 -0400

    refactor navigator bar for get a new visual style

---
+++ lib/shared/presentation/widgets/custom_bottom_navigator_bar.dart
@@ -0,0 +1,127 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
+import '../../../config/theme/colors/colors_values.dart';
+import '../../../navigation/navigation_controller.dart';
+
+class CustomBottomNavigatorBar extends StatelessWidget {
+  const CustomBottomNavigatorBar({
+    super.key,
+    required this.navigationController,
+    required this.pageButtons,
+  });
+
+  final NavigationController navigationController;
+  final List pageButtons;
+
+  @override
+  Widget build(BuildContext context) => Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 1.5, sigmaY: 1.5),
+        child: Obx(
+          () => Container(
+            height: 64,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: ColorValues.textPrimaryInv(context).withOpacity(0.5),
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(
+                color: ColorValues.utilityInfo(context).withAlpha(120),
+                width: 1.25,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: ColorValues.utilityInfo(context).withAlpha(150),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: pageButtons
+                  .map(
+                    (e) => Expanded(
+                      child: _CustomBottomNavigatorItem(
+                        onTap: () => navigationController.changeIndex(
+                          pageButtons.indexOf(e),
+                        ),
+                        content: e,
+                        isSelected:
+                            pageButtons.indexOf(e) ==
+                            navigationController.selectedIndex.value,
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+class _CustomBottomNavigatorItem extends StatelessWidget {
+  const _CustomBottomNavigatorItem({
+    required this.content,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  final Map<String, dynamic> content;
+  final bool isSelected;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+    onTap: () => onTap(),
+    behavior: HitTestBehavior.opaque,
+    child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AnimatedBar(isActive: isSelected),
+          Opacity(
+            opacity: isSelected ? 1 : 0.85,
+            child: Icon(
+              isSelected
+                  ? (content['active_icon'] ?? content['icon'])
+                  : content['icon'],
+              size: 24,
+              color: isSelected
+                  ? ColorValues.fgBrandSecondary(context)
+                  : ColorValues.utilityGrey500(context),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+class AnimatedBar extends StatelessWidget {
+  const AnimatedBar({super.key, required this.isActive});
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) => AnimatedContainer(
+    margin: const EdgeInsets.only(bottom: 4),
+    duration: const Duration(milliseconds: 200),
+    height: 4,
+    width: isActive ? 24 : 0,
+    decoration: BoxDecoration(
+      color: ColorValues.fgBrandSecondary(context),
+      borderRadius: BorderRadius.circular(16),
+    ),
+  );
+}
